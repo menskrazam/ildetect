73a67ac4565e7f9d230eb4fad25100faef6051ff
theghoully <theghoully@protonmail.com>
2021-05-22 13:17:15 +0200

    User 100 user agent strings

--- tasks.js
+++ tasks.js
@@ -3,17 +3,7 @@ const got = require('got');
 const eggs = require('./easterEggsDomains.json');
 const iploggerServers = require('./iploggerDomains.json');
 const lookup = require('safe-browse-url-lookup');
-
-const client = got.extend(
-  {
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
-      Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-      'Accept-Encoding': 'gzip, deflate',
-      'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8'
-    }
-  }
-);
+const userAgents = require('./userAgents.json')
 
 // Init google safe browser
 const googleLookup = lookup({ apiKey: process.env.GOOGLE_API_KEY });
@@ -128,6 +118,19 @@ const isIpLoggerUrl = (url) => {
 };
 
 const fetchPage = async (url) => {
+  const userAgent = userAgents[Math.floor(Math.random() * userAgents.length)];
+
+  const client = got.extend(
+    {
+      headers: {
+        'User-Agent': userAgent,
+        Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8'
+      }
+    }
+  );
+
   return client(url, {
     timeout: 10000,
     hooks: {
